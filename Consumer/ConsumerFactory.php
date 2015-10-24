95a9d8ce9935fdc59604a10a77d1bc9bddec9bbe
jonathan Stidwill <jonathan.stidwill@vouchedfor.co.uk>
2015-10-24 07:29:33 +0100

    Added factory

---
+++ Consumer/ConsumerFactory.php
@@ -0,0 +1,36 @@
+<?php
+namespace Vouchedfor\SegmentIOBundle\Consumer;
+
+/**
+ *  * Class ConsumerFactory
+ *   * @package Vouchedfor\SegmentIOBundle\Consumer
+ *    */
+class ConsumerFactory
+{
+
+    /**
+     * @param $type
+     * @param $access_token
+     * @return null|File|\Vouchedfor\SegmentIOBundle\Consumer\Socket
+     * @throws \Exception
+     */
+    public static function createConsumer($type, $access_token)
+    {
+        $instance = null;
+
+        switch ($type) {
+            case 'socket':
+                $instance = new Socket($access_token);
+                break;
+
+            case 'file':
+                $instance = new File($access_token);
+                break;
+
+            default:
+                throw new \Exception('Invalid consumer type');
+        }
+
+        return $instance;
+    }
+}
